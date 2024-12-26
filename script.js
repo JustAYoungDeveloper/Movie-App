7ee7d1a0ad9fc0c07c86c3a3592fbf3b4ae128f5
Solat <solatpukhtunyar123@gmail.com>
2024-12-26 12:12:35 +0500

    Heavily edited functions and more in script.js.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const searchForm = document.querySelector('form');
-const moviecontainer = document.querySelector('.movies-container');
+const moviecontainer = document.querySelector('.movie-container');
 const inputBox = document.querySelector('.inputBox');
 
 // Function to acquire movie info through API
@@ -14,18 +14,26 @@ const getMovieInfo = async (movie) =>{
 
 // Function to show movie info
 const showMovieData = (data) => {
-    const {Title, Year, Rated, Released, Runtime, Genre, imdbRating, Director, Writer, Actors, Plot, Language, Country, Awards, Poster, Ratings} = data
+    moviecontainer.innerHTML = '';
+    const {Title, Year, Rated, Released, Runtime, Genre, imdbRating, Actors, Plot, Country, Poster, Ratings} = data
     const movieDiv = document.createElement('div')
+    movieDiv.classList.add('movie-info');
     movieDiv.innerHTML = `<h2>${Title}</h2>
                             <p><strong>Rating: &#11088;</strong>${imdbRating}</p>`;
     const GenreDiv = document.createElement('div');
     GenreDiv.classList.add('movie-genre');
 
-    Genre.split(',').forEach(genre => {
+    GenreDiv.split(',').forEach(genre => {
         const genreP = document.createElement('p');
         genreP.innerText = genre
         GenreDiv.appendChild(genreP);
     });
+    movieDiv.appendChild(GenreDiv);
+
+    movieDiv.innerHTML += `<p><strong>Release date: </strong>${Released}</p>
+                            <p><strong>Duration: </strong>${Runtime}</p>
+                            <p><strong>Actors: </strong>${Actors}</p>
+                            <p><strong>Plot: </strong>${Plot}</p>`;
     moviecontainer.appendChild(movieDiv);
 }
 

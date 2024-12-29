a18bfef00b0be335829c5dfccb2808e01031e078
Solat <solatpukhtunyar123@gmail.com>
2024-12-29 11:15:09 +0500

    Added some code for even  better user experience

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ const inputBox = document.querySelector('.inputBox');
 
 // Function to fetch the movie data
 const getMovieInfo = async (movie) => {
+    try {
     const apiKey = "705c0f44"
     const url = `https://www.omdbapi.com/?t=${movie}&apikey=${apiKey}`;
     const response = await fetch(url);
 
+    if (!response.ok) {
+        throw new Error('Unable to fetch the movie data.');
+    }
     const data = await response.json();
     showMovieData(data);
+    }     
+    catch (error) {
+        showErrorMessage('Movie not found');    
+    }
+
 }
 // Show the movie data
 const showMovieData = (data) => {
@@ -46,16 +55,24 @@ const showMovieData = (data) => {
     moviecontainer.appendChild(movieElement);
 }
 
-//  Add event listener to the movie container
-searchForm.addEventListener('submit', async (e) => {
+// Function to display error message
+const showErrorMessage = (message) => {
+    moviecontainer.innerHTML = `<h2>${message}</h2>`;
+    moviecontainer.classList.add('noBackground');
+}
+// Function to handle the form submission
+const handleFormSubmission = (e) => {
     e.preventDefault();
     console.log(inputBox.value);
     const movieName = inputBox.value.trim();
     if (movieName !== '') {
+        showErrorMessage('Fetching the movie data...');
         getMovieInfo(movieName);
     }
     else {
-        moviecontainer.innerHTML = `<h2>Please enter a movie name</h2>`;
-        moviecontainer.classList.add('noBackground');
+        showErrorMessage('Please enter a movie name.');
     }
-});
+}
+
+//  Add event listener to the movie container
+searchForm.addEventListener('submit', handleFormSubmission);
